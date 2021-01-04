bdcbf81cfd2316444c6bab729c2ad7fdb742fb33
LOVE <2902692127@qq.com>
2021-01-04 11:32:50 +0800

    1

---
+++ src/bean/ScoreDAO.java
@@ -0,0 +1,11 @@
+package bean;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+
+public class ScoreDAO {
+
+}
---
+++ src/bean/ScoreDTO.java
@@ -0,0 +1,49 @@
+package bean;
+
+public class ScoreDTO {
+    private String courseName;
+    private String studentId;
+    private String courseXf;
+    private String score;
+    private String jige;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getCourseXf() {
+        return courseXf;
+    }
+
+    public void setCourseXf(String courseXf) {
+        this.courseXf = courseXf;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public String getJige() {
+        return jige;
+    }
+
+    public void setJige(String jige) {
+        this.jige = jige;
+    }
+}
